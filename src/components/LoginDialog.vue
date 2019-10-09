aad258744eaaad6fbd52d9ca2ab56094a67f40d0
Lachlan McWilliam <mcwilliam01@gmail.com>
2019-10-10 00:26:18 +0100

    Added login functionality and protected routes

---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-dialog v-model="dialog" overlay-color="green" max-width="600px">
+    <template v-slot:activator="{ on }">
+      <v-btn color="primary" class="font-weight-bold" text v-on="on">Login</v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline" color="primary">Login</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-form ref="form">
+            <v-text-field
+              label="E-mail*"
+              name="username"
+              type="text"
+              prepend-inner-icon="mdi-face"
+              required
+              v-model="email"
+              :rules="emailRules"
+            ></v-text-field>
+            <v-text-field
+              label="Password*"
+              name="password"
+              type="password"
+              prepend-inner-icon="mdi-lock"
+              required
+              v-model="password"
+              :rules="passwordRules"
+            ></v-text-field>
+            <v-checkbox v-model="rememberMe" color="primary" label="Remember me"></v-checkbox>
+          </v-form>
+        </v-container>
+        <small>*indicates required field</small>
+      </v-card-text>
+      <v-card-actions>
+        <div class="flex-grow-1"></div>
+        <v-btn color="error" text v-on:click="dialog = false">cancel</v-btn>
+        <v-btn color="primary" :loading="this.loggingIn" v-on:click="loginSubmit">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  data: () => ({
+    //Component data
+    dialog: false,
+    email: "",
+    password: "",
+    rememberMe: false,
+
+    //Form rules
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    passwordRules: [v => !!v || "Password is required"]
+  }),
+  computed: {
+    //Vuex state variables
+    ...mapState(["loggingIn", "loginError", "loginSuccessful"])
+  },
+  methods: {
+    //Vuex state actions
+    ...mapActions(["doLogin", "doLoginSaveAccess", "getLoginStatus"]),
+    loginSubmit() {
+      if (this.$refs.form.validate() && this.rememberMe) {
+        let logginPromise = new Promise((resolve, reject) => {
+          this.doLoginSaveAccess({
+            email: this.email,
+            password: this.password
+          });
+          if (this.getLoginStatus()) {
+            resolve("Login successful");
+          } else {
+            reject("Login failed");
+          }
+        }).then(message => {
+          console.log(message);
+          this.checkLoginStatus(this.getLoginStatus());
+        });
+      } else if (this.$refs.form.validate() && !this.rememberMe) {
+        let logginPromise = new Promise((resolve, reject) => {
+          this.doLogin({
+            email: this.email,
+            password: this.password
+          });
+          if (this.getLoginStatus()) {
+            resolve("Login successful");
+          } else {
+            reject("Login failed");
+          }
+        }).then(message => {
+          console.log(message);
+          this.checkLoginStatus(this.getLoginStatus());
+        });
+      }
+    },
+    checkLoginStatus(status) {
+      if (status) {
+        console.log("loggin worked! also hello!");
+        this.dialog = false;
+        this.$router.push("home");
+      }
+    }
+  }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,25 +1,53 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import store from "./store";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
+const router = new Router({
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/welcome",
+      name: "welcome",
+      component: () => import("./views/Welcome.vue")
+    },
+    {
+      path: "*",
+      redirect: "/welcome"
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  store.dispatch("fetchAccessToken");
+  if (to.fullPath === "*") {
+    if (!store.state.accessToken) {
+      next("/welcome");
+    }
+  }
+  if (to.fullPath === "/welcome") {
+    if (store.state.accessToken) {
+      next("/");
+    }
+  }
+  next();
+});
+
+export default router;
